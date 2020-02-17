532d72d513ccb5f0cab93889d5e450a72a7a6a0d
Johnson Kow <jkow95@gmail.com>
2020-02-17 01:11:57 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   movies = directors_database[0][:movies]#[0][:title]   #JAWS
   
   i = 0 
-  while i < directors_database[0][:movies]
+  while i < directors_database[0][:movies].length 
     puts directors_database[0][:movies][i][:title]
     i += 1 
   end 
